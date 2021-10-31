6d6e3bce5e11caa15434fdc64af0aca217e76e2e
GustavoCarv <gustavo.carvalho@econverse.com.br>
2021-10-31 20:21:36 -0300

    Parte inicial de apresentação e projetos startada

---
+++ portfolio_website-STARTER/src/components/Projects/Projects.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
+import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import { projects } from '../../constants/constants';
+
+const Projects = () => (
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle>
+        Projetos
+    </SectionTitle>
+    <GridContainer>
+      {projects.map(({id,image,description,tags,source,visit, title}) => {
+        return (
+        <BlogCard key={id}>
+          <Img src={image}/>
+          <TitleContent>
+            <HeaderThree title>{title}</HeaderThree>
+          </TitleContent>
+          <CardInfo>{description}</CardInfo>
+          <div>
+            <TitleContent>
+              Stacks
+            </TitleContent>
+            <TagList>
+              {tags.map((tag, i) => {
+                return (
+                <Tag key={i}>
+                  {tag}
+                </Tag>)
+              })}
+            </TagList>
+          </div>
+        </BlogCard>
+        )
+      })}
+    </GridContainer>
+  </Section>
+
+);
+
+export default Projects;
